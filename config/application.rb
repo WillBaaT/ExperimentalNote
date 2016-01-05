d440a41695b9b7d5170e3314d719f7391571b871
Will Chen <albumen1985@gmail.com>
2016-01-05 22:52:27 +0800

    'configissue'

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,6 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 module ExperimentalNote
   class Application < Rails::Application
-    config.serve_static_files = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
