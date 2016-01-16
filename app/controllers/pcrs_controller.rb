6bef7ccf023579bf4e136d32d306c06ddcb50bde
Will Chen <albumen1985@gmail.com>
2016-01-16 15:47:49 -0800

    pcr input ok

--- app/controllers/pcrs_controller.rb
+++ app/controllers/pcrs_controller.rb
@@ -2,30 +2,29 @@ class PcrsController < ApplicationController
   def index
   	@gene = Gene.all
   	@pcr = Pcr.all
-  	#if @gene.nil?
-  	#	redirect_to pcr_path
-  	#end
+  	# if Gene.first.nil?
+  	# 	redirect_to new_pcr_path
+  	# end
   end
 
   def new
-    @gene = Gene.new
-    @gene.pcrs.build #build a blank pcrs form
+    @pcr = Pcr.new
+    @pcr.build_gene #build a blank pcrs form
   end
  
   def create
-    # if Gene.find_by_name(params[:gene][:name]) 
-    #   params[:pcrs][:gene_id] = Gene.find(Gene.find_by_name(params[:gene][:name]).id)
-    #    @gene = Gene.find(Gene.find_by_name(params[:gene][:name]).id)
-    # else
-    #   @gene = Gene.new(gene_params)
-    # end
-    if @gene.save
-    redirect_to pcrs_path
-    else
-    render new_pcr_path
+    @pcr = Pcr.new(pcr_params)
+    if  Gene.find_by_name(@pcr.gene.name)
+      @pcr.gene = Gene.find(Gene.find_by_name(@pcr.gene.name).id)
+      @pcr.save
     end
-  
+    if @pcr.save
+      redirect_to pcrs_path
+    else
+      render new_pcr_path
+    end
   end
+
   def show
   	
   end
@@ -40,14 +39,8 @@ class PcrsController < ApplicationController
   end
 
   private
-  def gene_params
-  	params.require(:gene).permit(
-      :id,
-      :species,
-      :number,
-      :name,
-      :size,
-  		:pcrs_attributes => [
+  def pcr_params
+      params.require(:pcr).permit(
       :gene_id,
       :fragment_name, 
       :product_size,
@@ -65,7 +58,41 @@ class PcrsController < ApplicationController
       :polya,
       :polya_t,
       :cycles,
+      :gene_attributes => [
+      :id,
+      :species,
+      :number,
+      :name,
+      :size,
       :_destroy]
-	)
+  )
   end
+ #  def gene_params
+ #  	params.require(:gene).permit(
+ #      :id,
+ #      :species,
+ #      :number,
+ #      :name,
+ #      :size,
+ #  		:pcrs_attributes => [
+ #      :gene_id,
+ #      :fragment_name, 
+ #      :product_size,
+ #      :final_vector,
+ #      :machine,
+ #      :enzyme,
+ #      :break,
+ #      :break_t,
+ #      :denature,
+ #      :denature_t,
+ #      :annealing,
+ #      :annealing_t,
+ #      :elongation,
+ #      :elongation_t,
+ #      :polya,
+ #      :polya_t,
+ #      :cycles,
+ #      :_destroy]
+	# )
+ #  end
 end
